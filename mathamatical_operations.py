ed76fa3a27a28dd262a6c613f5268d986a10a3c5
VivGOAT4444 <khuranaviv@gmail.com>
2026-08-28 20:08:41 +0100

    Add files via upload

---
+++ mathamatical_operations.py
@@ -0,0 +1,17 @@
+import math
+
+print(math.ceil(23.56))
+# 24
+
+print(math.floor(23.56))
+# 23
+
+print(math.copysign(10, -15))
+# -10.0
+
+print(math.fabs(-96))
+# 96.0
+
+print(math.gcd(24, 56))
+# 8
+
